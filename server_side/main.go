0212f8b51c5b0d6530f2c9ec80498197c71fd304
Tsubasa Ozawa <t.ozawa.sake@gmail.com>
2020-09-22 16:35:26 +0900

    echoの基本

--- server_side/main.go
+++ server_side/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-    "fmt"
+	"go-mod-test/handler"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-    fmt.Println("hello")
+	// Echoのインスタンス生成
+	e := echo.New()
+
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/hello", handler.MainPage())
+
+	// サーバー起動
+	e.Start(":8080") //ポート番号指定してね
 }
